1390f60709aebeca9f799cd588ae2ddd0fc4d6b5
Earth <earthhadjo@gmail.com>
2020-06-30 23:37:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+const url = 'http://127.0.0.1/:8000/api/shoe/?format=json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      earthshoes: [{shoe: 'sneaker'}, {shoe: "sandal"}, {shoe: 'boot'}, {shoe: "dress"}, {shoe: "other"}]
+    };
+  }
+  componentDidMount() {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({earthshoes: data});
+      console.log(data);
+    });
+  }
+
+  render() {
+    // const { earthshoes = [] } = this.state;
+    return (
+      <div>
+        <h3>Earth1ne Shoe World!</h3>
+        {this.state.earthshoes.map((shoe) => {
+          return(
+
+            <ul>
+           <li>
+             Size: {shoe.size}
+           </li>
+           <li>
+             Brand: {shoe.brand}
+           </li>
+           <li>
+             Manufacturer: {shoe.manufacturer} 
+            </li>
+            <li>
+             Color: {shoe.color}
+          </li>
+          <li>
+            Material: {shoe.material}
+          </li>
+          <li>
+            Shoe Type: {shoe.shoe_type}
+          </li>
+          <li>
+            Fasten Type: {shoe.fasten_type}
+          </li>
+         </ul> 
+        )
+        })}
+        </div>
+        );
+      } 
 }
 
 export default App;
